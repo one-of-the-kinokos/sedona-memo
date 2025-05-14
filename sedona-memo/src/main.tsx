0aa5f98a63f02d5dd15f9a53760024d8f911925c
KINOKO_MAN <ogaki.viator.masashi@gmail.com>
2025-05-14 13:02:09 +0900

    fix

--- sedona-memo/src/main.tsx
+++ sedona-memo/src/main.tsx
@@ -28,5 +28,5 @@ const rootElement = document.getElementById("app")!;
 
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(<RouterProvider router={router} basepath="sedona-memo" />);
+  root.render(<RouterProvider router={router} basepath="/sedona-memo" />);
 }
